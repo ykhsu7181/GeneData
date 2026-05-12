79078f4189502b8e4229f7e426ba450845c7a9e1
ykx <ykhsu7181@gmai.com>
2026-05-12 10:56:25 +0800

    Remove integrations app from Django project

--- django2/filemanager/urls.py
+++ django2/filemanager/urls.py
@@ -1,4 +1,4 @@
-"""filemanager URL Configuration
+﻿"""filemanager URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
-from files.views import GenomeFileViewSet  # 添加导入
+from files.views import GenomeFileViewSet
 from files import views
-
 from django.http import JsonResponse
+
 
 def debug_view(request):
     """Debug view to test if Django is receiving requests"""
@@ -30,20 +30,16 @@ def debug_view(request):
         'headers': dict(request.headers)
     })
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gd/api/files/', include('files.urls')),
-    path('gd/api/admin/', include('files.urls')),  # 添加管理后台API路由
+    path('gd/api/admin/', include('files.urls')),
     path('api/files/', include('files.urls')),
-    path('api/admin/', include('files.urls')),  # 添加管理后台API路由（通过代理）
+    path('api/admin/', include('files.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('docs/', include_docs_urls(title='文件管理API文档')),
-
-    # Debug endpoint to test if Django is receiving requests
     path('gd/api/debug/', debug_view, name='debug'),
-
-    # 添加兼容旧版路径的路由
     path('gd/api/download/', GenomeFileViewSet.as_view({'get': 'download_transcriptome'}), name='legacy-download'),
-    # manual_download功能
     path('gd/api/manual_download/<str:filename>', views.download_manual_file, name='legacy-manual-download'),
 ]
